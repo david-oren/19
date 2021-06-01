8c10d827a2c96979baa8652fc9665ff09852c5d5
David Oren <daoren@mi-r3-p1.s19.be>
2021-06-01 15:50:29 +0200

    print f

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,29 @@
 /*   By: daoren <daoren@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 13:21:07 by daoren            #+#    #+#             */
-/*   Updated: 2021/05/06 13:21:21 by daoren           ###   ########.fr       */
+/*   Updated: 2021/06/01 15:49:33 by daoren           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+typedef struct	s_printf_data
+{
+	va_list	argc;
+	char	current_char;
+	char	*current_str;
+	int		minus;
+	int		dot;
+	int		zero;
+	int		width;
+	int		precision;
+}				t_printf_data;
+
+
+#endif
